54e8e0bc75af71c001e3d09d1fc31b218978f55e
qja7767 <101855116+qja7767@users.noreply.github.com>
2022-07-28 10:36:41 +0900

    Add files via upload

---
+++ banking/src/main/java/com/varxyz/banking/service/AccountService.java
@@ -0,0 +1,14 @@
+package com.varxyz.banking.service;
+
+import java.util.List;
+
+import com.varxyz.banking.domain.Account;
+
+public interface AccountService {
+	
+	void addAccounts(Account account);
+	List<Account> getAccounts(String userId);
+	List<Account> getAllAccount();
+	void doTransfer(double money, String withdrawAccountNum, String depositAccountNum);
+	double getBalance(String accountNum);
+}
