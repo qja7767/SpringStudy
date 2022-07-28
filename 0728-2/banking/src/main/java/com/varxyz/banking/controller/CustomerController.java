8a394e6cfbf5bc3205e5f89738dcd673dc2bccb6
kgb <qlcjs7789@naver.com>
2022-07-28 17:53:43 +0900

    d

---
+++ 0728-2/banking/src/main/java/com/varxyz/banking/controller/CustomerController.java
@@ -0,0 +1,38 @@
+package com.varxyz.banking.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.varxyz.banking.domain.Customer;
+import com.varxyz.banking.domain.CustomerCommand;
+import com.varxyz.banking.service.CustomerServiceImpl;
+
+@Controller
+public class CustomerController {
+	@Autowired
+	CustomerServiceImpl customerService;
+	
+	@GetMapping("login/add_customer")
+	public String addCustomerForm() {
+		return "/login/add_customer";
+	}
+	
+	@PostMapping("login/add_customer")
+	public String addCustomer(CustomerCommand customerCommand, Model model) {
+		model.addAttribute("customerCommand", customerCommand);	
+		Customer customer = new Customer();
+		customer.setEmail(customerCommand.getEmail());
+		customer.setPasswd(customerCommand.getPasswd());
+		customer.setName(customerCommand.getName());
+		customer.setSsn(customerCommand.getSsn());
+		customer.setPhone(customerCommand.getPhone());
+		customerService.addCustomer(customer);
+		return "login/add_success_customer";
+	}
+	
+	
+	
+}
---
+++ 0728-2/banking/src/main/webapp/WEB-INF/views/account/add_account.jsp
@@ -0,0 +1,52 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet" href="<c:url value='/resources/css/assets/dist/css/bootstrap.min.css'/>">
+<link rel="stylesheet" type="text/css" href="<c:url value='/resources/css/css_header/headers.css'/>">
+</head>
+
+<body>
+	<jsp:include page="/resources/css/incl/header.jsp"/>
+${userId}님 계좌타입을 선택해 주세요.
+<form action="add_account" method="post">
+	<input type="text" name="accType"/>
+	<input type="submit" value="신청"/>
+</form>
+
+<br>
+이메일로 계좌찾기
+<form action="find_account" method="post">
+	<input type="submit" value="조회"/>
+</form>
+
+<br>
+모든 가입유저 계좌찾기(관리자)
+<form action="find_all_account" method="post">
+	<input type="submit" value="조회"/>
+</form>
+
+<br>
+송금 시스템(transfer)
+<form action="transfer1" method="get">
+	<input type="submit" value="송금"/>
+</form>
+
+<br>
+입금
+<form action="withdraw" method="get">
+	<input type="submit" value="입금"/>
+</form>
+
+<br>
+출금
+<form action="deposit" method="get">
+	<input type="submit" value="출금"/>
+</form>
+
+</body>
+</html>
