228b0ee2c24c109dfe07b2ac0312a70f2434bdf6
kgb <qlcjs7789@naver.com>
2022-07-28 10:34:42 +0900

    ddddd
    dd

---
+++ kgb/src/main/java/com/varxyz/banking/AccountDao.java
@@ -0,0 +1,58 @@
+package com.varxyz.banking;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Date;
+import java.util.List;
+import static java.sql.Types.*;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+public class AccountDao {
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public AccountDao(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	//신규계좌 추가
+	public void addAccount(Account account) {
+		String sql = "INSERT INTO Account (accountNum, accType, balance,"
+				+ " interestRate, overAmount, customerId)"
+				+ " VALUES (?, ?, ?, ?, ?, ?)";
+		SavingsAccount sa = null;
+		CheckingAccount ca = null;
+		Object[] args = null;
+		int[] types = new int[] {CHAR, CHAR, DOUBLE, DOUBLE, DOUBLE, BIGINT};
+		
+		if(account instanceof SavingsAccount) {
+			sa = (SavingsAccount)account;
+			args = new Object[] {
+				sa.getAccountNum(), String.valueOf(sa.getAccountType()),
+				sa.getBalance(), sa.getInterestRate(), 0.0,
+				0
+			};
+		}else {
+			ca = (CheckingAccount)account;
+			args = new Object[] {
+				ca.getAccountNum(), String.valueOf(ca.getAccountType()),
+				ca.getBalance(), ca.getOverdraftAmount(), 0.0,
+				0
+			};
+		}
+		jdbcTemplate.update(sql, args, types);
+	}
+	
+	
+	
+}
